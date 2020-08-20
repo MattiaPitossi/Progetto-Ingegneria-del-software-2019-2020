c26c4d00a09c94c3c70287860c0d824b7f15c809
MattiaPitossi <mattiapitossi@MacBook-Pro-di-Mattia.local>
2020-08-21 01:29:36 +0200

    Modifiche menu manutentore

--- VERSIONE 1/Versione 1/view/MenuManutentore.java
+++ VERSIONE 1/Versione 1/view/MenuManutentore.java
@@ -12,7 +12,7 @@ import utility.MyMenu;
 
 public class MenuManutentore {
     final private static String TITOLO = "Sistema domotico";
-    final private static String [] VOCIMENU = {"Crea categoria sensore", "Crea nuova categoria attuatore", "Crea nuovo sensore (richiede la presenza di almeno una categoria)"};
+    final private static String [] VOCIMENU = {"Crea categoria sensore", "Crea nuova categoria attuatore", "Crea nuovo sensore (richiede la presenza di almeno una categoria)", "Crea nuovo attuatore (richiede la presenza di almeno una categoria)","Crea nuova unita' immobiliare","Descrivi unita' immobiliare" };
     final private static String MESS_USCITA = "Vuoi veramente uscire ?";
     final private static String ERRORE_FUNZIONE = "La funzione non rientra tra quelle disponibili !";
     final private static String MESS_ALTRA_OPZIONE = "Selezionare un'altra opzione.";
@@ -78,30 +78,38 @@ public class MenuManutentore {
         case 4: // Crea nuovo attuatore (solo se esiste almeno una categoria di attuatore)
           break;
 
-        case 5: // Crea nuova unità immobiliare (unica per questa versione, verificare che non sia già presente)
-          boolean finito = false;
+        case 5: // Crea nuova unita' immobiliare (unica per questa versione, verificare che non sia già presente)
+          boolean finitoStanze = false;
+          boolean finitoArtefatti = false;
           if(alreadyCreatedUnit == 1){
             System.out.println("Unita' immobiliare gia' creata!");
           } else {
             UnitaImmobiliare unitaImmobiliare = new UnitaImmobiliare();
-            String nomeUnita = InputDati.leggiStringa("Inserisci nome unita' immobiliare");
+            String nomeUnita = InputDati.leggiStringa("Inserisci nome unita' immobiliare: ");
             unitaImmobiliare.setNomeUnita(nomeUnita);
             if(unitaImmobiliare.aggiungiStanza(InputDati.leggiStringa("Inserisci il nome della prima stanza: "))) 
               System.out.println("Stanza aggiunta correttamente");
             do{
               String nomeStanzeSuccessive = InputDati.leggiStringa("Inserisci il nome della stanza(fine per uscire): ");
-              if(nomeStanzeSuccessive.equals("fine")) finito = true;
+              if(nomeStanzeSuccessive.equals("fine")) finitoStanze = true;
               else if(unitaImmobiliare.aggiungiStanza(nomeStanzeSuccessive)) 
                 System.out.println("Stanza aggiunta correttamente");
-            } while(!finito);
+            } while(!finitoStanze);
             if(unitaImmobiliare.aggiungiArtefatto(InputDati.leggiStringa("Inserisci il nome del primo artefatto: "))) 
             System.out.println("Artefatto aggiunto correttamente");
+            do{
+              String nomeArtefattiSuccessivi = InputDati.leggiStringa("Inserisci il nome della stanza(fine per uscire): ");
+              if(nomeArtefattiSuccessivi.equals("fine")) finitoArtefatti = true;
+              else if(unitaImmobiliare.aggiungiArtefatto(nomeArtefattiSuccessivi)) 
+                System.out.println("Artefatto aggiunto correttamente");
+            } while(!finitoArtefatti);
             alreadyCreatedUnit = 1;
+            System.out.println("La creazione dell'unita' immobiliare e' completata!");
           }
           
           break;
 
-        case 6: // Descrivi unità immboliliare (verificare che sia stata creata)
+        case 6: // Descrivi unità immobiliare (verificare che sia stata creata)
           break;
     
         default: // Se i controlli nella classe Menu sono corretti, questo non viene mai eseguito !
